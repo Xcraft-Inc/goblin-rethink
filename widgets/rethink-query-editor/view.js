d74fb959581a35b7baa3610a15a53f98488d3d79
Samuel Loup <samuel.loup@gmail.com>
2021-05-26 22:32:14 +0200

    Refactor Editor in a new component, use editor for results

--- widgets/rethink-query-editor/view.js
+++ widgets/rethink-query-editor/view.js
@@ -5,53 +5,27 @@ import Widget from 'goblin-laboratory/widgets/widget';
 import View from 'goblin-laboratory/widgets/view';
 import Container from 'goblin-gadgets/widgets/container/widget.js';
 import Button from 'goblin-gadgets/widgets/button/widget.js';
-import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
-import prettier from 'prettier';
-
-class ConsoleLineNC extends Widget {
-  constructor() {
-    super(...arguments);
-  }
-
-  render() {
-    const {index, line} = this.props;
-    return (
-      <React.Fragment>
-        {index} {line}
-        <br />
-      </React.Fragment>
-    );
-  }
-}
-
-const ConsoleLine = Widget.connect((state, prop) => {
-  const line = state.get(`backend.${prop.id}.lines[${prop.index}]`);
-  return {line};
-})(ConsoleLineNC);
+import GoblinEditor from '../goblin-editor/widget.js';
 
 class ConsoleNC extends Widget {
   constructor() {
     super(...arguments);
+    this.editorElement = null;
+    this.assign = this.assign.bind(this);
+  }
+
+  assign(component) {
+    this.editorElement = component;
+  }
+
+  componentDidUpdate() {
+    const {lines, printStatus} = this.props;
+    const source = `[${lines.join(',\n')}]\n//${printStatus}`;
+    this.editorElement.setSource(source);
   }
 
   render() {
-    const {id, lines, printStatus} = this.props;
-    const console = {
-      font: '16px/1 monospace',
-      whiteSpace: 'break-spaces',
-      overflow: 'auto',
-      height: '100%',
-      width: '100%',
-      userSelect: 'text',
-    };
-    return (
-      <div style={console}>
-        {lines.map((l, k) => (
-          <ConsoleLine id={id} key={k} index={l} />
-        ))}
-        <small style={{userSelect: 'text'}}>{printStatus}</small>
-      </div>
-    );
+    return <GoblinEditor ref={this.assign} source={''} />;
   }
 }
 
@@ -61,18 +35,20 @@ const Console = Widget.connect((state, prop) => {
   if (!lines) {
     return {lines: [], printStatus: ''};
   }
-  return {lines: Array.from(lines.keySeq()), printStatus};
+  return {lines, printStatus};
 })(ConsoleNC);
 class RethinkQueryEditor extends Widget {
   constructor() {
     super(...arguments);
-    this.assign = this.assign.bind(this);
-    this.init = this.init.bind(this);
     this.update = this.update.bind(this);
     this.run = this.run.bind(this);
-    this.format = this.format.bind(this);
     this.save = this.save.bind(this);
     this.editorElement = undefined;
+    this.assign = this.assign.bind(this);
+  }
+
+  assign(component) {
+    this.editorElement = component;
   }
 
   run() {
@@ -80,86 +56,12 @@ class RethinkQueryEditor extends Widget {
   }
 
   save() {
-    this.format();
+    this.editorElement.format();
     this.do('save');
   }
 
   update(value) {
     this.do('update', {src: value});
-  }
-
-  format() {
-    let src = this.model.getValue();
-    try {
-      src = prettier.format(src, {parser: 'babel'});
-      this.model.setValue(src);
-    } catch (err) {
-      console.error(err.stack);
-      this.editor.deltaDecorations(
-        [],
-        [
-          {
-            range: new monaco.Range(3, 1, 3, 1),
-            options: {
-              isWholeLine: true,
-              className: 'prettierError',
-              glyphMarginClassName: 'prettierError',
-            },
-          },
-        ]
-      );
-    }
-  }
-
-  assign(component) {
-    this.editorElement = component;
-  }
-
-  componentDidMount() {
-    this.init();
-  }
-
-  componentWillUnmount() {
-    this.destroy();
-  }
-
-  destroy() {
-    if (this.editor) {
-      this.editor.dispose();
-      const model = this.editor.getModel();
-      if (model) {
-        model.dispose();
-      }
-    }
-    this._subscription && this._subscription.dispose();
-  }
-
-  init() {
-    const templateSrc = this.props.source;
-
-    const model = monaco.editor.createModel(templateSrc, 'javascript');
-    this.model = model;
-    this.format();
-    this._subscription = model.onDidChangeContent(() => {
-      const src = model.getValue();
-      this.props.onValueChange(src);
-      this.update(src);
-    });
-
-    this.editor = monaco.editor.create(this.editorElement, {
-      language: 'javascript',
-      lineNumbers: 'on',
-      scrollbar: {
-        vertical: 'auto',
-        horizontal: 'auto',
-      },
-      theme: 'vs',
-      automaticLayout: true,
-    });
-
-    this.editor.setModel(model);
-    this.update(templateSrc);
-    monaco.editor.setTheme('vs');
   }
 
   render() {
@@ -203,13 +105,13 @@ class RethinkQueryEditor extends Widget {
         </Container>
         <Container kind="row" height="100%" grow="1">
           <Container kind="column" width="100%" height="100%" grow="1">
-            <div
-              id="editor"
-              style={{width: '100%', height: '100%'}}
+            <GoblinEditor
               ref={this.assign}
+              source={this.props.source}
+              onUpdate={this.update}
             />
           </Container>
-          <Container kind="column" height="80%" width="100%">
+          <Container kind="column" width="100%" height="100%" grow="1">
             <Console id={this.props.id} />
           </Container>
         </Container>
@@ -226,7 +128,6 @@ const EditorLoaderNC = (props) => {
     <RethinkQueryEditor
       id={props.id}
       desktopId={props.desktopId}
-      onValueChange={() => null}
       jobId={props.jobId}
       name={props.name}
       source={props.source}
